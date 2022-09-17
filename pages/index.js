cbe48516b41d333c4fc18fe6127d7777b3a620d0
Reuban Bryenton <r.bryenton2003@gmail.com>
2022-09-17 14:27:07 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,11 @@ export default function Home() {
     return (
         <>
             <Hero />
+            <div className='h-screen flex justify-center items-center bg-base-100'>
+                <h1 className='text-5xl text-primary font-bold'>
+                    SOON
+                </h1>
+            </div>
         </>
     )
 }
