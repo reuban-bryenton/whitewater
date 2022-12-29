462b138cb39920d578b9e6393f4189081071b579
Reuban Bryenton <r.bryenton2003@gmail.com>
2022-12-29 01:17:20 +0100

    PWA support

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+    dest: 'public',
+    register: true,
+    sw: 'service-worker.js',
+    skipWaiting: true,
+    publicExcludes: [
+        '!robots.txt',
+        '!sitemap.xml.gz',
+    ],
+});
+
+module.exports = withPWA({
     reactStrictMode: true,
     swcMinify: true,
     i18n: {
         locales: ['en'],
         defaultLocale: 'en',
     },
-}
-
-module.exports = nextConfig
+});
