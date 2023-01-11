7aa3d581db7b73e2dbd3ceb5061d0f7f69e5ce72
Reuban Bryenton <r.bryenton2003@gmail.com>
2023-01-11 23:43:15 +0100

    Create video.js

---
+++ pages/video.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <video autoPlay loop muted className="absolute z-10 w-auto min-w-full min-h-full max-w-none">
+            <source src="videos/splash.mp4" type="video/mp4" />
+        </video>
+    )
+}
